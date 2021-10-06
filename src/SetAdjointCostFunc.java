dc95f5965366e0c5b9a08dcfc5a340d747c0d13f
Zhang <zhan3907@purdue.edu>
2021-10-05 22:34:15 -0400

    cost function overhaul has been completed
    Only one cost function exist now, it's called Cost Function. Adjoint specific report also exists now, this class messes with it to get cl or cd

--- src/SetAdjointCostFunc.java
+++ src/SetAdjointCostFunc.java
@@ -8,6 +8,7 @@ import star.flow.ForceCoefficientReport;
 public class SetAdjointCostFunc extends StarMacro {
 
 	private static String costFuncReportName = "Adjoint Cost Function";
+	private static String costFuncName = "Cost Function";
 
 	public void execute() {
 		
@@ -56,19 +57,17 @@ public class SetAdjointCostFunc extends StarMacro {
 	}
 
 	/**
-	 * Remove existing cost functions if there are any
+	 * Remove existing cost functions not named costFuncName if there are any
 	 * @param sim
 	 */
 	private void removeExisting(SimComponents sim) {
 		
 		Collection<AdjointCostFunction> costFuncs = sim.activeSim.get(AdjointCostFunctionManager.class).getObjects();
-		StarPlot plot = sim.activeSim.getPlotManager().getPlot("Residuals");
-		
-		// Remove existing cost function only if it doesn't have existing solutions
+
 		for (AdjointCostFunction f:costFuncs) {
-			double[] res = plot.getDataSetManager().getDataSet(f.getPresentationName() + "::Residual").getYValues();
-			if (res.length == 0)
+			if (!f.getPresentationName().equals(costFuncName)){
 				sim.activeSim.get(AdjointCostFunctionManager.class).removeObjects((ReportCostFunction) f);
+			}
 		}
 		
 	}
@@ -78,44 +77,13 @@ public class SetAdjointCostFunc extends StarMacro {
 	 * @param sim
 	 */
 	private void setCostFunc(SimComponents sim) {
-		
-        String costFunc = SimComponents.valEnvString("adjoint_cost_func");
-        ReportCostFunction reportCostFunc = sim.activeSim.get(AdjointCostFunctionManager.class).createAdjointCostFunction(ReportCostFunction.class);
-        
-        // don't create new cost function if there's a existing cost fucntion for the same report
+
+        // create cost functions if only there are no cost function set
         Collection<AdjointCostFunction> costFuncs = sim.activeSim.get(AdjointCostFunctionManager.class).getObjects();
-        boolean hasCl = false, hasCd = false, hasLD = false;
-        for (AdjointCostFunction f:costFuncs) {
-        	
-        	String name = f.getPresentationName();
-        	if (name.equals("Lift Coefficient"))
-        		hasCl = true;
-        	if (name.equals("Drag Coefficient"))
-        		hasCd = true;
-			/*
-        	if (name.equals("L/D"))
-        		hasLD = true;
-			 */
-        }
-        
-        
-		if (costFunc.equals(SimComponents.ADJOINT_COST_FUNC_CL) && !hasCl) {
-			reportCostFunc.setPresentationName("Lift Coefficient");
-			reportCostFunc.setReport(sim.activeSim.getReportManager().getReport("Lift Coefficient"));
-		} else if (costFunc.equals(SimComponents.ADJOINT_COST_FUNC_CD) && !hasCd){
-			reportCostFunc.setPresentationName("Drag Coefficient");
-			reportCostFunc.setReport(sim.activeSim.getReportManager().getReport("Drag Coefficient"));
-		/*
-		} else if (costFunc.equals(SimComponents.ADJOINT_COST_FUNC_LD) && !hasLD) {
-			reportCostFunc.setPresentationName("L/D");
-			reportCostFunc.setReport(sim.activeSim.getReportManager().getReport("L/D"));
-		 */
-		} else if (hasCl || hasCd || hasLD) {
-			sim.activeSim.println("Existing cost function of the same type detected. No new cost function created.");
-		} else {
-			sim.activeSim.println("Error! No cost function selected. Killing sim.");
-			sim.killSim();
+		if (costFuncs.size() == 0) {
+			ReportCostFunction reportCostFunc = sim.activeSim.get(AdjointCostFunctionManager.class).createAdjointCostFunction(ReportCostFunction.class);
+			reportCostFunc.setReport(sim.activeSim.getReportManager().getReport("Adjoint Cost Function"));
+			reportCostFunc.setPresentationName(costFuncName);
 		}
 	}
-	
 }
