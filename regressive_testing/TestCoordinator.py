b0fec68beebc061acccc61dcd677d920106fca1f
1208780949 <fredzhang1208780949@gmail.com>
2021-11-09 13:12:45 -0500

    test environment detection has been tested. Log writing is a bit buggy with new lines

--- regressive_testing/TestCoordinator.py
+++ regressive_testing/TestCoordinator.py
@@ -4,9 +4,14 @@ from datetime import datetime
 #       Initialization
 # -----------------------------
 
-log_time = datetime.utcnow()
-log_file = open(log_time.strftime("regressive_testing_%Y%m%d%h%m%s.txt"), "w")
+# version of basesim this regression test is written for
+VERSION = 5.2
 
+# initialize log file
+log_time = datetime.utcnow()
+log_file = open(log_time.strftime("regressive_testing_%Y%m%d%H%M%S.txt"), "w")
+
+# test environments
 GEOMETRY_PREP = [
     "DomainSet.java",
     "RideHeight.java",
@@ -40,7 +45,8 @@ INITIAL_EXECUTION = [
 ]
 
 LATE_STAGE_EXECUTION = [
-    "run.java"
+    "run.java",
+    "ConvergenceChecker.java"
 ]
 
 POST_PROC = [
@@ -54,8 +60,10 @@ POST_PROC = [
 # -----------------------------
 
 
-def write_log(message, file=log_file):
-    file.write(message)
+def write_log(message="", extra_blank_line=False, file=log_file):
+    file.write(message + "\n")
+    if extra_blank_line:
+        file.write("\n")
 
 
 def fatal_error(message, file=log_file):
@@ -64,42 +72,109 @@ def fatal_error(message, file=log_file):
 
 
 def log_run_date():
-    # update the log file that records when the regression suite was last ran
+    # update the log file that records when the regression suite was last ran and the version of basesim it ran on
+
+    write_log("Documenting when this program has been ran...")
+
     f = open("last_run_date.txt", "w")
     now = datetime.utcnow()
-    f.write(now.strftime("%Y-%m-%dT%H:%M:%SZ"))
+    f.write(now.strftime("%Y-%m-%dT%H:%M:%SZ\n"))
+    f.write("VERSION = " + str(VERSION))
     f.close()
 
+    write_log("Logged time: " + now.strftime("%Y-%m-%dT%H:%M:%SZ"))
+    write_log("Logged version: " + str(VERSION), True)
 
-def det_test_env(file_list):
+
+def get_test_env(file_list):
     # takes in a list of java file that has been changed and determin what tests to run
-    test_envs = []
+    # TO-DO: add integration for complete test
+
+    write_log("Determining test environments to run...")
+
+    envs = []
     for file in file_list:
-        if file in GEOMETRY_PREP and GEOMETRY_PREP not in test_envs:
-            test_envs.append(GEOMETRY_PREP)
-        elif file in GEOMETRY_REPAIR and GEOMETRY_REPAIR not in test_envs:
-            test_envs.append(GEOMETRY_REPAIR)
-        elif file in MESH_PREP and MESH_PREP not in test_envs:
-            test_envs.append(MESH_PREP)
-        elif file in MESH and MESH not in test_envs:
-            test_envs.append(MESH)
-        elif file in MESH_REPAIR and MESH_PREP not in test_envs:
-            test_envs.append(MESH_REPAIR)
-        elif file in INITIAL_EXECUTION and INITIAL_EXECUTION not in test_envs:
-            test_envs.append(INITIAL_EXECUTION)
-        elif file in LATE_STAGE_EXECUTION and LATE_STAGE_EXECUTION not in test_envs:
-            test_envs.append(LATE_STAGE_EXECUTION)
-        elif file in POST_PROC and POST_PROC not in test_envs:
-            test_envs.append(POST_PROC)
+        if file in GEOMETRY_PREP:
+            if GEOMETRY_PREP not in envs:
+                envs.append(GEOMETRY_PREP)
+        elif file in GEOMETRY_REPAIR:
+            if GEOMETRY_REPAIR not in envs:
+                envs.append(GEOMETRY_REPAIR)
+        elif file in MESH_PREP:
+            if MESH_PREP not in envs:
+                envs.append(MESH_PREP)
+        elif file in MESH:
+            if MESH not in envs:
+                envs.append(MESH)
+        elif file in MESH_REPAIR:
+            if MESH_PREP not in envs:
+                envs.append(MESH_REPAIR)
+        elif file in INITIAL_EXECUTION:
+            if INITIAL_EXECUTION not in envs:
+                if file == "run.java":
+                    # if run.java got changed, run both initial_execution and late_stage_execution envs
+                    envs.append(LATE_STAGE_EXECUTION)
+                else:
+                    envs.append(INITIAL_EXECUTION)
+        elif file in LATE_STAGE_EXECUTION:
+            if LATE_STAGE_EXECUTION not in envs:
+                envs.append(LATE_STAGE_EXECUTION)
+        elif file in POST_PROC:
+            if POST_PROC not in envs:
+                envs.append(POST_PROC)
         else:
-            fatal_error("File changed not found in testing environments")
-    return test_envs
+            fatal_error("File " + file + " not found in testing environments")
+
+    # logging test environments
+    write_log("These environments will be ran: ")
+    for env in envs:
+        if env == GEOMETRY_PREP:
+            write_log("GEOMETRY_PREP")
+        elif env == GEOMETRY_REPAIR:
+            write_log("GEOMETRY_REPAIR")
+        elif env == MESH_PREP:
+            write_log("MESH_PREP")
+        elif env == MESH:
+            write_log("MESH")
+        elif env == MESH_REPAIR:
+            write_log("MESH_REPAIR")
+        elif env == INITIAL_EXECUTION:
+            write_log("INITIAL_EXECUTION")
+        elif env == LATE_STAGE_EXECUTION:
+            write_log("LATE_STAGE_EXECUTION")
+        elif env == POST_PROC:
+            write_log("POST_PROC")
+        else:
+            fatal_error("Something weird has happened with testing environments")
+    write_log(extra_blank_line=True)
+
+    return envs
+
+
+def get_files_changed():
+    # TO-DO: actually write it (git integration). This is just for testing for now
+
+    write_log("Detecting files changed...")
+
+    files = ["run.java", "ExportReports.java", "MeshRepair.java", "ExportReports.java", "PostProc.java"]
+
+    # logging files changed
+    write_log("Files changes detected:")
+    for file in files:
+        write_log(file)
+    write_log(extra_blank_line=True)
+
+    return files
 
 
 # ----------------------------
 #       Execution
 # -----------------------------
 
+files_changed = get_files_changed()
+test_envs = get_test_env(files_changed)
+
 # exit the program
+log_run_date()  # only log run date if the program finished executing seems to make sense, we will see
 write_log("Regressive testing complete...exiting")
 exit(0)
