c2db195e8626b9f7d4c510a08031a430ed8b6a3d
Zhang <zhan3907@purdue.edu>
2021-11-23 18:35:44 -0500

    debug

--- src/RTTestController.java
+++ src/RTTestController.java
@@ -82,6 +82,7 @@ public class RTTestController extends StarMacro {
      */
     private void sortParts() {
 
+        sim.println("hi");
         Collection<GeometryPart> geomParts = sim.getGeometryPartManager().getParts(); // get all parents
 
         // sort CFD, tire, aero, chassis, and suspension parts
