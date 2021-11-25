cfcd6f5df79280444a5b57eaf62ae594ebdb79f7
Zhang <zhan3907@purdue.edu>
2021-11-25 16:29:54 -0500

    got the centroid of the car in standard conditon

--- src/RTRideHeight.java
+++ src/RTRideHeight.java
@@ -1,9 +1,3 @@
-import star.base.report.ExpressionReport;
-import star.base.report.SumReport;
-import star.common.PartSurface;
-
-import java.util.Collection;
-
 /**
  * Ride height testing. Generate the centroid x, y, z report values of all CFD parts and tires at (0,0) ride height.
  * Lower the ride height to (-1, -1), generate the centroid x, y, z report values of CFD parts and tire. x and y values
@@ -11,6 +5,12 @@ import java.util.Collection;
  * Raise the ride height to (-1, 0) and generate centroid x, y, and z report values of CFD parts tire. y values should
  * not change, x should move forward a little, and z should be increased accordingly. Tire centroid should not change.
  */
+import star.base.report.ExpressionReport;
+import star.base.report.SumReport;
+import star.common.PartSurface;
+
+import java.util.Collection;
+
 public class RTRideHeight {
 
     private RTTestComponent rt;
@@ -34,10 +34,23 @@ public class RTRideHeight {
     SumReport sumArea;
 
     public RTRideHeight(RTTestComponent rt) {
-        this.rt = rt;
 
+        // initialization
+        this.rt = rt;
+        double[] stdTire = new double[3]; // centroid of tire parts in standard condition in inches
+        double[] stdCFD = new double[3]; // centroid of CFD parts in standard condition in inches
+        double[] rhTire = new double[3]; // centroid of tire parts in RH -1 condition in inches
+        double[] rhCFD = new double[3]; // centroid of CFD parts in RH -1 condition in inches
+        double[] pitchTire = new double[3]; // centroid of tire parts in (-1, 0) condition in inches
+        double[] pitchCFD = new double[3]; // centroid of CFD parts in (-1, 0) condition in inches
         this.initReports();
+
+        // standard conditions
+        this.setReportPartToTire();
+        stdTire = this.getCentroid();
         this.setReportPartToCFD();
+        stdCFD = this.getCentroid();
+
     }
 
     /**
@@ -77,6 +90,21 @@ public class RTRideHeight {
         this.sumYPosArea.getParts().setObjects(surfaces);
         this.sumZPosArea.getParts().setObjects(surfaces);
         this.sumArea.getParts().setObjects(surfaces);
+
+    }
+
+    /**
+     * Get the centroid value in inches by simply reading the reports. This method does NOT do any setup. It's dumb, it
+     * just reads the reports
+     * @return x, y, z in inches
+     */
+    private double[] getCentroid() {
+
+        double[] centroid = new double[3];
+        centroid[0] = this.centroidXReport.getValue();
+        centroid[1] = this.centroidYReport.getValue();
+        centroid[2] = this.centroidZReport.getValue();
+        return centroid;
 
     }
 
