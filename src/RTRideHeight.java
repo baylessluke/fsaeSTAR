3fc8e3bd16b5b85017a963af39e53695b48cbe7a
Zhang <zhan3907@purdue.edu>
2021-11-29 16:28:59 -0500

    debug

--- src/RTRideHeight.java
+++ src/RTRideHeight.java
@@ -328,7 +328,7 @@ public class RTRideHeight {
 
         // the new location of the point
         double[] newLoc = new double[3];
-        newLoc[0] = r * Math.cos(pNewCSh) - csLoc[0];
+        newLoc[0] = r * Math.cos(pNewCSh) - Math.abs(csLoc[0]);
         newLoc[2] = r * Math.sin(pNewCSh) + csLoc[2];
 
         // get the delta
