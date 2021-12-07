e0e269a049166e99e9d9d41a00320439b7297e7e
rkumaran <rkumaran@purdue.edu>
2021-12-06 20:21:35 -0500

    new files

---
+++ starRecs/exportMuffler.java
@@ -0,0 +1,13 @@
+// Simcenter STAR-CCM+ macro: exportMuffler.java
+// Written by Simcenter STAR-CCM+ 16.06.008
+package macro;
+
+import java.util.*;
+
+import star.common.*;
+
+public class exportMuffler extends StarMacro {
+
+  public void execute() {
+  }
+}
